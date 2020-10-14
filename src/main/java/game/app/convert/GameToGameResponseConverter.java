dbd9ffd29dd290d860d0ad607250c6acdc9cf501
MakiWazowski <irenebenitopinel@gmail.com>
2020-10-14 10:47:36 +0200

    Initial commit

--- src/main/java/game/app/convert/GameToGameResponseConverter.java
+++ src/main/java/game/app/convert/GameToGameResponseConverter.java
@@ -1,5 +1,15 @@
 package game.app.convert;
 
-public class GameToGameResponseConverter {
+import org.springframework.core.convert.converter.Converter;
+import game.app.dtos.request.GameDto;
+import game.app.entities.Game;
+
+public class GameToGameResponseConverter implements Converter<Game,GameDto>{
+
+	@Override
+	public GameDto convert(Game source) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
